39966ce31eb84bb3c0917e260584c5e8669ae2a9
Jzbonner <jarrett.bonner@gmail.com>
2019-12-23 15:25:18 -0500

    changes to readme

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-/**Puppeteer Initialization */
+/**This web scraper pulls in popular stock price data from CNN */
+
+/**Puppeteer initialization */
 const puppeteer = require("puppeteer"); 
 const browserFetcher = puppeteer.createBrowserFetcher({ platform: 'linux' });
 const revision = require('puppeteer/package').puppeteer.chromium_revision;
@@ -7,26 +9,32 @@ browserFetcher.download(revision)
     .then(() => console.log('Done, with revision number: ', revision))
     .catch(error => console.log('Error', error));
 
-/**Headless Browser Functionality */
+/**Headless browser functionality */
 async function main() {
     const browser = await puppeteer.launch({
-        headless: true,
+        headless: false, // -> this is set to false for visual purposes
         executablePath: 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe', 
     })
-    let stockURL = "https://money.cnn.com/data/hotstocks/index.html";
-        
+    let stockURL = "https://money.cnn.com/data/hotstocks/index.html";    
     const page = await browser.newPage(); 
     await page.goto(stockURL);
+    
 
-    await page.evaluate(() => {
+    let data = await page.evaluate(() => {
         let stockAbbr = document.querySelector('td a[class="wsod_symbol"]').innerText; 
         let stockName = document.querySelector('td a[class="wsod_symbol"] + span').innerText; 
         let stockPrice = document.querySelector('td[class="wsod_aRight"] > span').innerText; 
-        console.log(stockAbbr + ' ' + stockName + ' ' + stockPrice); 
-        debugger; 
+        return {
+            stockAbbr, 
+            stockName, 
+            stockPrice
+        }
+        
     });
 
-    
+    /**Displays returned data in debug console*/
+    console.log(data); 
+    await browser.close();
 }; 
 
 main(); 
