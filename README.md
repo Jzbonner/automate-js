244945f93c9bc6ad52484b47abccf80f7d475ebb
Jzbonner <jarrett.bonner@gmail.com>
2019-12-23 15:09:43 -0500

    intro to web scraping

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ## 💻 Automate-js
 ![automation-bg](https://i.imgur.com/o39eifM.jpg?1)
-
 A collection of JavaScript applications that aid in automating everyday work-flow task. Automation can  allow for proficient daily operations from sales to tech support. It is my aim to create a suite of lightweight products and tools that industry professionals can adopt into their daily setups. 
 
 ### Tools 
---
+++ index.js
@@ -0,0 +1,32 @@
+/**Puppeteer Initialization */
+const puppeteer = require("puppeteer"); 
+const browserFetcher = puppeteer.createBrowserFetcher({ platform: 'linux' });
+const revision = require('puppeteer/package').puppeteer.chromium_revision;
+
+browserFetcher.download(revision)
+    .then(() => console.log('Done, with revision number: ', revision))
+    .catch(error => console.log('Error', error));
+
+/**Headless Browser Functionality */
+async function main() {
+    const browser = await puppeteer.launch({
+        headless: true,
+        executablePath: 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe', 
+    })
+    let stockURL = "https://money.cnn.com/data/hotstocks/index.html";
+        
+    const page = await browser.newPage(); 
+    await page.goto(stockURL);
+
+    await page.evaluate(() => {
+        let stockAbbr = document.querySelector('td a[class="wsod_symbol"]').innerText; 
+        let stockName = document.querySelector('td a[class="wsod_symbol"] + span').innerText; 
+        let stockPrice = document.querySelector('td[class="wsod_aRight"] > span').innerText; 
+        console.log(stockAbbr + ' ' + stockName + ' ' + stockPrice); 
+        debugger; 
+    });
+
+    
+}; 
+
+main(); 
